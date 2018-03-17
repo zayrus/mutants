160026075d25e574ce6b80e36180282df94941e5
Ernest <zayrus@gmail.com>
2018-03-17 15:57:00 -0300

    feat(controller): add /mutants endpoint
    /mutants endpoint analyze dna and return true or false

---
+++ api/controllers/analyze.js
@@ -0,0 +1,94 @@
+'use strict'
+
+function isMutant(dna) {
+	const matrixDna = dna.map(function(obj) {
+			return Object.keys(obj).sort().map(function(key) {
+					return obj[key]
+			})
+	})
+
+	const dnaA = ['A', 'A', 'A', 'A']
+	const dnaC = ['C', 'C', 'C', 'C']
+	const dnaG = ['G', 'G', 'G', 'G']
+	const dnaT = ['T', 'T', 'T', 'T']
+	const dnaAJoin = dnaA.join()
+	const dnaCJoin = dnaC.join()
+	const dnaGJoin = dnaG.join()
+	const dnaTJoin = dnaT.join()
+	let dnaToAnalize
+	let sequenceCount = 0
+	let i, j, k
+	let matrixLength = matrixDna.length
+	// Look for this combination horizontally. 
+	for (i = 0; i < matrixLength; i++) {
+			for (j = 0; j <= matrixDna[i].length - dnaA.length; j++) {
+					dnaToAnalize = []
+					for (k = 0; k < dnaA.length; k++) {
+							dnaToAnalize.push(matrixDna[i][j + k])
+					}
+					if (dnaToAnalize.join() === dnaAJoin || 
+							dnaToAnalize.join() === dnaCJoin || 
+							dnaToAnalize.join() === dnaGJoin ||
+							dnaToAnalize.join() === dnaTJoin ) {
+									sequenceCount++
+					}
+			}
+	}
+
+	// Look for this combination vertically. 
+	for (i = 0; i < matrixDna[0].length; i++) {
+			for (j = 0; j <= matrixLength - dnaA.length; j++) {
+					dnaToAnalize = []
+					for (k = 0; k < dnaA.length; k++) {
+							dnaToAnalize.push(matrixDna[j + k][i])
+					}
+					if (dnaToAnalize.join() === dnaAJoin || 
+							dnaToAnalize.join() === dnaCJoin || 
+							dnaToAnalize.join() === dnaGJoin ||
+							dnaToAnalize.join() === dnaTJoin ) {
+									sequenceCount++
+					}
+			}
+	}
+
+	// Look for this combination diagonally. 
+	for (i = 0; i <= matrixLength - dnaA.length; i++) {
+			for (j = 0; j <= matrixDna[i].length - dnaA.length; j++) {
+					dnaToAnalize = []
+					for (k = 0; k < dnaA.length; k++) {
+							dnaToAnalize.push(matrixDna[i + k][j + k])
+					}
+					if (dnaToAnalize.join() === dnaAJoin || 
+							dnaToAnalize.join() === dnaCJoin || 
+							dnaToAnalize.join() === dnaGJoin ||
+							dnaToAnalize.join() === dnaTJoin ) {
+									sequenceCount++
+					}
+			}
+	}
+
+	// and diagonally the other way... 
+	for (i = 0; i <= matrixLength - dnaA.length; i++) { 
+			for (j = matrixDna[i].length-1 ; j >= 0 + dnaA.length-1; j--) {
+					dnaToAnalize = []
+					for (k = 0; k < dnaA.length; k++) {
+							dnaToAnalize.push(matrixDna[i + k][j - k])
+					}
+					if (dnaToAnalize.join() === dnaAJoin || 
+							dnaToAnalize.join() === dnaCJoin || 
+							dnaToAnalize.join() === dnaGJoin ||
+							dnaToAnalize.join() === dnaTJoin ) {
+									sequenceCount++
+					}
+			}
+	}
+
+	console.log('Count ', sequenceCount)
+	if (sequenceCount >= 2) {
+	return true  //mutant
+	} else {
+	return false //human
+	}
+}
+
+module.exports = isMutant
--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const fastify = require('fastify')()
+'use strict'
 
-fastify.register(require("fastify-swagger"), {
+const fastify = require('fastify')()
+const fastifySwagger = require('fastify-swagger')
+const isMutant = require('./api/controllers/analyze')
+
+fastify.register(fastifySwagger, {
   mode: 'static',
   specification: {
     path: "./api/swagger/swagger.yaml"
   },
   exposeRoute: true
-});
+})
+
+fastify.post('/mutants', async (request, reply) => {
+  const analisys = isMutant(request.body.dna)
+  const responseCode = analisys ? 200 : 403
+  reply.type('application/json').code(responseCode)
+  return analisys
+})
+
+fastify.get('/stats', async (request, reply) => {
+  reply.type('application/json').code(200)
+  return { hello: 'world' }
+})
 
 fastify.listen(3000, '127.0.0.1', function (err) {
   if (err) throw err
