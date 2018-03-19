4f162588b28248496ccd41dba3864c1baef5b6fb
Ernest <zayrus@gmail.com>
2018-03-19 00:46:43 -0300

    fix(api): root endpoint
    change static response

--- routes.js
+++ routes.js
@@ -41,7 +41,7 @@ async function routes (fastify, options) {
 
 	fastify.get('/', async (request, reply) => {
 		reply.type('application/json').code(200)
-		return { hello: 'world' }
+		return { hello: 'Magneto' }
 	})
 }
 
